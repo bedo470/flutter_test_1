eba8260967b0e8933c2a4eea28d9824c4f6411b7
Abdelzaher <bedouwk688@gmail.com>
2023-04-24 06:28:10 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: dead_code
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: facebook(),
+    );
+  }
+}
+
+class facebook extends StatelessWidget {
+  const facebook({super.key});
+
+  @override
+  // ignore: dead_code, dead_code
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("facebook",
+            style: TextStyle(
+              color: Colors.blue[900],
+              fontSize: 25,
+            )),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.blue[900],
+            size: 25,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.message,
+                color: Colors.blue[900],
+                size: 25,
+              ),
+              onPressed: () {}),
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.blue[900],
+              size: 25,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
